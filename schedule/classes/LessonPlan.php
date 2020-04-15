9f007b4285de16b1bdce9fd796a2ecbc965384c2
YaroslavTos <63033614+YaroslavTos@users.noreply.github.com>
2020-04-15 16:42:17 +0600

    Add files via upload

--- schedule/classes/LessonPlan.php
+++ schedule/classes/LessonPlan.php
@@ -12,7 +12,9 @@ class LessonPlan extends Table
     public function validate()
     {
         try {
-            if (!empty($this->gruppa_id) && !empty($this->subject_id) && !empty($this->user_id)) {
+            if (!empty($this->gruppa_id) &&
+                !empty($this->subject_id) &&
+                !empty($this->user_id)) {
                 return true;
             } else {
                 throw new Exception('Не переданны все параметры');
--- schedule/classes/LessonPlanMap.php
+++ schedule/classes/LessonPlanMap.php
@@ -39,6 +39,24 @@ lesson_plan.user_id=$id");
         return false;
     }
 
+    public function  findTeachers($ofset=0, $limit=30){
+        $res = $this->db->query("SELECT user.user_id,CONCAT(user.lastname,' ', user.firstname, ' ',
+user.patronymic) AS fio,"
+            . " otdel.name AS otdel,
+COUNT(lesson_plan.subject_id) AS
+count_plan,SUM(subject.hours) AS sum_hours "
+            . "FROM user INNER JOIN teacher ON
+user.user_id=teacher.user_id INNER JOIN otdel "
+            . "ON teacher.otdel_id=otdel.otdel_id
+LEFT OUTER JOIN lesson_plan ON
+teacher.user_id=lesson_plan.user_id "
+            . "LEFT OUTER JOIN subject ON
+lesson_plan.subject_id=subject.subject_id GROUP BY
+user.user_id "
+            . "LIMIT $ofset, $limit");
+        return $res->fetchAll(PDO::FETCH_OBJ);
+    }
+
 
 
     public function delete($id){
